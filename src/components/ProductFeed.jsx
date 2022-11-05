cb4298084245401b4e940e6fdba97c3651e377c6
Hugo Ramon <ramone.techie@gmail.com>
2022-11-05 00:02:28 -0300

    Fixed product card layout issues

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -1,19 +1,57 @@
+import Image from "next/image";
 import Product from "./Product";
 
 const ProductFeed = ({ products }) => {
   return (
-    <div className="grid grid-flow-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-44">
-      {products.map(({ id, title, price, description, category, image }) => (
-        <Product
-          key={id}
-          id={id}
-          title={title}
-          price={price}
-          description={description}
-          category={category}
-          image={image}
-        />
-      ))}
+    <div className="grid grid-flow-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <Image
+        src="/amazon-wide.jpeg"
+        className="md:col-span-full"
+        width={1550}
+        height={50}
+        alt="Wide image"
+      />
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ export default function Home({ products }) {
   return (
     <div>
       <Head>
-        <title>Amazon 2.0</title>
+        <title>Amazon</title>
       </Head>
       <Header />
       <main className="max-w-screen-2xl mx-auto bg-gray-100">
